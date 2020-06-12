8b32b61dfee1a30dc422eb8cae3a9ca57ca556b5
brett-macneil <brett.macneil@dal.ca>
2020-06-12 11:56:22 -0300

    Added external field widget

--- symmetric_extfield_widget.py
+++ symmetric_extfield_widget.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 10 2020
+Created on Fri Jun 12 11:24:50 2020
 
 @author: Brett
 """
@@ -33,7 +33,7 @@ ax2.set_ylabel(r'Magnetization (kA m$^{-1}$)', fontsize=16)
 ### Constants
 ###______________________________________________________________
 
-numpoints = 200           # Number of points used in equation solver
+numpoints = 50           # Number of points used in equation solver
 
 # Physical constants
 mu0 = 4*np.pi*1e-7        # Permeability of free space
@@ -88,23 +88,26 @@ def brillouin(y, J):
     return B
 
 
-def mag_eq_a(Mb, lambda_ab, T):
-    return Ma_max * brillouin(-mu0 * mua_max * lambda_ab * Mb / (kB*T), Ja)
+def mag_eq_a(Mb, lambda_ab, T, H):
+    return Ma_max * brillouin(mu0*mua_max*(H - lambda_ab*Mb) / (kB*T), Ja)
 
 
-def mag_eq_b(Ma, lambda_ab, T):
-    return Mb_max * brillouin(-mu0 * mub_max * lambda_ab * Ma / (kB*T), Jb)
+def mag_eq_b(Ma, lambda_ab, T, H):
+    return Mb_max * brillouin(mu0*mub_max*(H - lambda_ab*Ma) / (kB*T), Jb)
 
 
-def equations(mags, lambda_ab, T):
+def equations(mags, lambda_ab, T, H):
     Ma, Mb = mags
-    eq1 = mag_eq_a(Mb, lambda_ab, T) - Ma
-    eq2 = mag_eq_b(Ma, lambda_ab, T) - Mb
+    eq1 = mag_eq_a(Mb, lambda_ab, T, H) - Ma
+    eq2 = mag_eq_b(Ma, lambda_ab, T, H) - Mb
     return (eq1, eq2)
 
 
-def get_mag(T_min, T_max, numpoints, lambda_ab, kilo=True):
+def get_mag(T_min, T_max, numpoints, lambda_ab, H, kilo=True, bfield=True):
     #t_start = time.time()
+    
+    if bfield == True: 
+        H /= mu0
     
     Tvec = np.linspace(T_min, T_max, numpoints)
     Ma = np.empty(numpoints)
@@ -112,7 +115,7 @@ def get_mag(T_min, T_max, numpoints, lambda_ab, kilo=True):
     guess = [-Ma_max, Mb_max] # Initial guess
     
     for i in range(numpoints):
-        ma, mb = fsolve(equations, x0=guess, args=(lambda_ab, Tvec[i]))
+        ma, mb = fsolve(equations, x0=guess, args=(lambda_ab, Tvec[i], H))
         Ma[i] = ma; Mb[i] = mb
         guess = [ma, mb]
         
@@ -129,7 +132,7 @@ def get_mag(T_min, T_max, numpoints, lambda_ab, kilo=True):
 ###______________________________________________________________
 
 # Coupling constant
-lam_loc = plt.axes([0.125, 0.15, 0.775, 0.03])
+lam_loc = plt.axes([0.125, 0.10, 0.775, 0.03])
 lam_ab_init = 500.
 lam_ab_max = 800.
 lam_ab_min = 200.
@@ -138,7 +141,7 @@ lam_ab_sl = Slider(lam_loc, label=r'$\lambda_{ab}$', valmin=lam_ab_min, \
 lam_ab_sl.label.set_size(16)
 
 # Temperature
-T_loc = plt.axes([0.125, 0.1, 0.775, 0.03])
+T_loc = plt.axes([0.125, 0.05, 0.775, 0.03])
 T_init = 300.
 T_max = 600.
 T_min = 1.
@@ -146,6 +149,14 @@ T_sl = Slider(T_loc, label=r'$T$ (K)', valmin=T_min, valmax=T_max, \
               valinit=T_init)
 T_sl.label.set_size(16)
 
+# External field
+H_loc = plt.axes([0.125, 0.15, 0.775, 0.03])
+H_init = 0.
+H_max = 10.
+H_min = -10.
+H_sl = Slider(H_loc, label=r'$\mu_{0}H$ (T)', valmin=H_min, valmax=H_max, \
+              valinit=H_init)
+H_sl.label.set_size(16)
     
 ### Plots
 ###______________________________________________________________
@@ -154,18 +165,18 @@ T_sl.label.set_size(16)
 Ma_scale = np.linspace(-Ma_max, Ma_max, 100)
 Mb_scale = np.linspace(-Mb_max, Mb_max, 100)
 
-Ma_curve = mag_eq_a(Ma_scale, lam_ab_init, T_init)
-Mb_curve = mag_eq_b(Mb_scale, lam_ab_init, T_init)
+Ma_curve = mag_eq_a(Ma_scale, lam_ab_init, T_init, H_init)
+Mb_curve = mag_eq_b(Mb_scale, lam_ab_init, T_init, H_init)
 
 Ma_plot1, = ax1.plot(Mb_scale/1e3, Ma_curve/1e3, color='cyan')
 Mb_plot1, = ax1.plot(Mb_curve/1e3, Ma_scale/1e3, color='orange')
 
 # Magnetization-temperature subplot (Right, axis 2)
-Temp_vec, Mag_a, Mag_b = get_mag(T_min, T_max, numpoints, lam_ab_init)
+Temp_vec, Mag_a, Mag_b = get_mag(T_min, T_max, numpoints, lam_ab_init, H_init)
 
 Ma_plot2, = ax2.plot(Temp_vec, Mag_a, color='cyan')
 Mb_plot2, = ax2.plot(Temp_vec, Mag_b, color='orange')
-Mtot_plot2, = ax2.plot(Temp_vec, Mag_a+Mag_b, color='white', ls='dotted')
+Mtot_plot2, = ax2.plot(Temp_vec, Mag_a+Mag_b, color='white', zorder=10)
 Mag_min = min( min(Mag_a), min(Mag_b) )
 Mag_max = max( max(Mag_a), max(Mag_b) )
 
@@ -182,15 +193,17 @@ def update(val):
     # Pull val from sliders
     lam_ab_new = lam_ab_sl.val
     T_new = T_sl.val
+    H_new = H_sl.val
     
     # Update axis 1
-    Ma_curve_new = mag_eq_a(Ma_scale, lam_ab_new, T_new)
-    Mb_curve_new = mag_eq_b(Mb_scale, lam_ab_new, T_new)
+    Ma_curve_new = mag_eq_a(Ma_scale, lam_ab_new, T_new, H_new)
+    Mb_curve_new = mag_eq_b(Mb_scale, lam_ab_new, T_new, H_new)
     Ma_plot1.set_data(Mb_scale/1e3, Ma_curve_new/1e3)
     Mb_plot1.set_data(Mb_curve_new/1e3, Ma_scale/1e3)
     
     # Update axis 2
-    _, Mag_a_new, Mag_b_new = get_mag(T_min, T_max, numpoints, lam_ab_new)
+    _, Mag_a_new, Mag_b_new = get_mag(T_min, T_max, numpoints, lam_ab_new, \
+                                      H_new)
     Mag_min_new = min( min(Mag_a_new), min(Mag_b_new) )
     Mag_max_new = max( max(Mag_a_new), max(Mag_b_new) )
     Ma_plot2.set_ydata(Mag_a_new)
@@ -200,6 +213,7 @@ def update(val):
     Temp_line.set_ydata([Mag_min_new, Mag_max_new])
     
 
+H_sl.on_changed(update)
 lam_ab_sl.on_changed(update)
 T_sl.on_changed(update)
 fig.canvas.draw_idle()

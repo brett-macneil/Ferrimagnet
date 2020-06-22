4076deba2155539256e59139c1f8ea476a6ef65c
brett-macneil <brett.macneil@dal.ca>
2020-06-22 15:43:57 -0300

    Fixed self-consistent plot labeling

--- Widgets/neel_widget.py
+++ Widgets/neel_widget.py
@@ -23,8 +23,10 @@ plt.style.use('dark_background')
 #plt.style.use('lab')
 
 # Label axes
-ax1.set_xlabel(r'$M_{b}$ (kA m$^{-1}$)', fontsize=16)
-ax1.set_ylabel(r'$M_{a}$ (kA m$^{-1}$)', fontsize=16)
+ax1.set_xlabel(r'$M_{b}$ (Arb. Units)', fontsize=16)
+ax1.set_ylabel(r'$M_{a}$ (Arb. Units)', fontsize=16)
+ax1.set_xticklabels([])
+ax1.set_yticklabels([])
 
 ax2.set_xlabel('Temperature (K)', fontsize=16)
 ax2.set_ylabel(r'Magnetization (kA m$^{-1}$)', fontsize=16)
